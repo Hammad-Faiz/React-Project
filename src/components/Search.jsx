b7c8e0f8882e15bba9fabdaa08424154de19bb1d
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-07-13 22:18:17 -0400

    css update , checked code

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useState } from 'react'
-import {useNavigate} from 'react-router-dom'
-// import {FaSearch} from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+import {FaSearch} from 'react-icons/fa'
 
 
 function Search() {
 
-  const [input, setInput] = useState('')
-  const navigat = useNavigate();
+  const [input, setInput] = useState("")
+  const navigate = useNavigate();
  
   const submitHandler = (e) => {
     e.preventDefault();
-    navigat('./searched/' + input)
+    navigate('/searched/' + input)
   }
 
   return (
     <FormStyle onSubmit={submitHandler}>
       <div>
-        {/* <FaSearch></FaSearch> */}
+         <FaSearch></FaSearch>
         <input onChange={(e) => setInput(e.target.value)} type='text' value={input}/>
       </div>
     </FormStyle>
@@ -29,16 +29,17 @@ const FormStyle = styled.form`
   margin: 0rem 20rem;
   position: relative;
   width: 100px;
+
   input{
-  //   border: none;
-       background: linear-gradint(35deg, #494949, #313131);
-  //   font-size: 1.5rem;
-  //   color: white;
-  //   padding: 1rem 3rem;
-  //   border: none;
-  //   border-radius: 1rem;
-  //   outline: none;
-  width: 250%;
+     border: none;
+     background: linear-gradient(35deg, #494949, #313131);
+     font-size: 1.5rem;
+     color: white;
+     padding: 1rem 3rem;
+     border: none;
+     border-radius: 1rem;
+     outline: none;
+    width: 250%;
   }
 
   div {
